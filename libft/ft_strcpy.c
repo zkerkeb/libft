7d95a0f3b46f84c16227f95a608beb3be6b38e6d
hikkary <z.kerkeb@gmail.com>
2015-09-13 19:55:53 +0200

    Capitaine Teemo, au rapport !

---
+++ libft/ft_strcpy.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+char *strcpy(char *dest, const char *src)
+{
+	int i;
+
+	i = 0;
+
+	while(src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return(dest);
+}
