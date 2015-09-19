43979a3cd1311c088386745092d617a05ac4904e
hikkary <z.kerkeb@gmail.com>
2015-09-19 16:36:36 +0200

    Vous vous souviendrez de Yorick Mori !

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+void *ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t i;
+	unsigned char *d1;
+	const char *s1;
+	
+	d1 = (unsigned char *)dest;
+	s1 = (const char *)src;
+	i = 0;
+	while(i < n)
+	{
+		d1[i] = s1[i];
+		i++;
+	}
+	return((void *)d1);
+}
