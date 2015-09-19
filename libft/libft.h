e607009a4b6bb26d262ee0be5916ead0c1811a83
hikkary <z.kerkeb@gmail.com>
2015-09-19 22:06:31 +0200

    a jour

--- libft/libft.h
+++ libft/libft.h
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int ft_tolower(int c);
+int ft_toupper(int c);
 int ft_isprint(int c);
 int ft_isascii(int c);
 int ft_isalnum(int c);
@@ -11,12 +13,23 @@ int ft_isdigit(int c);
 int ft_isalpha(int c);
 int ft_atoi(const char *nptr);
 int ft_strcmp(const char *s1, const char *s2);
+char *ft_strchr(const char *s, int c);
+char *ft_strrchr(const char *s, int c);
+char *ft_strstr(const char *haystack, const char *needle);
+char *ft_strnstr(const char *haystack, const char *needle, size_t n);
 char *ft_strdup(const char *s);
+char *ft_strcat(char *dest, const char *src);
+char *ft_strncat(char *dest, const char *src, size_t n);
 char *ft_strncpy(char *dest, const char *src, size_t n);
 char *ft_strcpy(char *dest, const char *src);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t ft_strlen(const char *s);
+void *ft_memcpy(void *dest, const void *src, size_t n);
+void *ft_memccpy(void *dest, const void *src, int c, size_t n);
+void *ft_memset(void *s, int c, size_t n);
+void ft_putstr(char const *s);
 void ft_putnbr(int n);
 void ft_putchar(char c);
+void ft_bzero(void *s, size_t n);
 
 #endif 
--- libft/main.c
+++ libft/main.c
@@ -4,8 +4,20 @@
 
 int main()
 {
+	
+	//int c;
+	//c = 65;
+	unsigned char a[50] = "";
+	const char b[50] ="Wesh les beurettes";
 
+	printf("%p\n",ft_memccpy(a,b,'a',10));
+	printf("%p\n",memccpy(a,b,'e',10));
+	//printf("%p",memcpy(a,b,10));
+	//char d[50] = "lolee co acsdfsdfl lolelo;pe";
 	//ft_putnbr(ft_isalnum(45));
-	printf("%s",ft_strdup(""));
+	//ft_memset(s,c,3);
+	
+	
+	//ft_putstr(ft_strchr("lellee",'A'));
 	return(0);
 }
