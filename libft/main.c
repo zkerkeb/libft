b539e96c86851519f032473520aa5171eb77140c
hikkary <z.kerkeb@gmail.com>
2015-09-30 21:12:55 +0200

    Decision lumineuse

--- libft/main.c
+++ libft/main.c
@@ -3,21 +3,11 @@
 #include <stdlib.h>
 
 int main()
-{
-	
-	//int c;
-	//c = 65;
-	unsigned char a[50] = "";
-	const char b[50] ="Wesh les beurettes";
+{	
+	char str[10] = "aceeee";
+	char str2[10] = "aceee5";
 
-	printf("%p\n",ft_memccpy(a,b,'a',10));
-	printf("%p\n",memccpy(a,b,'e',10));
-	//printf("%p",memcpy(a,b,10));
-	//char d[50] = "lolee co acsdfsdfl lolelo;pe";
-	//ft_putnbr(ft_isalnum(45));
-	//ft_memset(s,c,3);
-	
-	
-	//ft_putstr(ft_strchr("lellee",'A'));
+	printf("%d",ft_strnequ(str,str2,5));
+
 	return(0);
 }
