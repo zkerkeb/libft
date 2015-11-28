c09232078662e753cc54d16c52f0ba897f8b70f5
Zakaria KERKEB <zkerkeb@e2r7p10.42.fr>
2015-11-28 21:44:22 +0100

    Mundo

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkerkeb <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/28 20:18:32 by zkerkeb           #+#    #+#             */
+/*   Updated: 2015/11/28 21:43:23 by zkerkeb          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*list;
+
+	list = (t_list *)malloc(sizeof(t_list));
+	if (list)
+	{
+		if (!content)
+		{
+			list->content = NULL;
+			list->content_size = 0;
+		}
+		else
+		{
+			list->content = malloc(content_size);
+			list->content = ft_memcpy(list->content, content, content_size);
+			list->content_size = &content_size;
+		}
+		list->next = NULL;
+	}
+	return (list);
+}
