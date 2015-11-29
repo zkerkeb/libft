5e335db4e5ce5a8fc6fba1a7e0fc53f58205a1cc
Zakaria KERKEB <zkerkeb@e3r7p18.42.fr>
2015-11-29 22:09:02 +0100

    Que volent mes fleches

---
+++ libft/ft_lstdel.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkerkeb <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/29 14:43:44 by zkerkeb           #+#    #+#             */
+/*   Updated: 2015/11/29 15:46:43 by zkerkeb          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	t_list *list;
+	
+	list = *alst;
+	while (list)
+	{
+		del(list->content, *list->content_size);
+		free(list);
+		list = list->next;
+	}
+	*alst = NULL;
+}
